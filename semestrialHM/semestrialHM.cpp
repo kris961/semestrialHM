fd5732d8e7873ca9039b33025483c6cef9b0084c
kris961 <59437063+kris961@users.noreply.github.com>
2021-12-23 19:58:08 +0200

    fully implemented saving data to binary file

--- semestrialHM/semestrialHM.cpp
+++ semestrialHM/semestrialHM.cpp
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 #include <string>
 #include <fstream>
+#include <windows.h>
 
 using namespace std;
-ifstream in_stream;
-ofstream out_stream;
-
 
 struct discipline {
-    string name;
+    char name[25];
     int grade;
 };
 
 struct student {
-    int facultyId;
-    string name;
+    int facultyId=0;
+    char name[30]="";
     discipline disciplines[6];
-    string sex;
-    int age;
-    string status;
+    char sex[10]="";
+    int age=0;
+    char status[10]="";
 };
 
 const int disciplinesCount = 6;
 const int groupCount = 30;
-const string fileName = "students.dat";
+const string fileName = "students.bin";
 
 int mainMenu();
 int addStudentMenu();
@@ -40,11 +38,21 @@ void readFromFile(student group[]);
 void main()
 {
     setlocale(LC_ALL, "Bulgarian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    bool hasChanges = false;
     bool end = false;
     int code;
-    student def = { 0 };
+    student def = { 0,"", {},"",0,"" };
     student group[30] = {def};
+    readFromFile(group);
     while (end != true) {
+        if (hasChanges) {
+            writeToFile(group);
+            readFromFile(group);
+            hasChanges = false;
+        }
         code = mainMenu();
         switch (code)
         {
@@ -64,8 +72,15 @@ void main()
                      for (int i = 0; i < n; i++)
                      {
                          createStudent(group);
+                         cout << "Желаете ли да въведете следващ ученик?    yes/no" << endl;
+                         string answer;
+                         cin >> answer;
+                         if (answer != "yes")
+                             break;
+                         system("CLS");
                      }
                  }
+                 hasChanges = true;
             }
             catch (const std::exception&)
             {
@@ -87,22 +102,21 @@ void main()
                 cout << "Въведете Факултетният номер на ученика." << endl;
                 cin >> fn;
                 found = addGrades(fn,group);
-                if (found==false)
+                if (found == false) {
                     cout << "Не присъства такъв студент в групата" << endl;
+                    cout << "Желаете ли да въведете оценки на друг студент? yes/no" << endl;
+                    string answer;
+                    cin >> answer;
+                    if (answer != "yes")
+                        break;
+                }
             } while (found==false);
+            hasChanges = true;
             break;
         }
         case 4: {
             end = true;
             continue;
-            break;
-        }
-        case 5: {
-            writeToFile(group);
-            break;
-        }
-        case 6: {
-            readFromFile(group);
             break;
         }
         default:
@@ -113,6 +127,7 @@ void main()
 }
 
 int mainMenu(){
+    system("CLS");
     int selection = 0;
         cout << "Въведете числото отговарящо на съответният ред" << endl;
         cout << "1) Добавяне студенти в група." << endl;
@@ -161,24 +176,19 @@ void createStudent(student group[]) {
     }
     student newStudent;
     newStudent.facultyId = facultyN;
-    newStudent.name = name;
-    newStudent.sex = sex;
-    newStudent.status = "учащ";
+    strcpy_s(newStudent.name, name.c_str());
+    strcpy_s(newStudent.sex, sex.c_str());
+    string status = "учащ";
+    strcpy_s(newStudent.status, status.c_str());
     newStudent.age = age;
 
-    discipline disciplines[12];
-    discipline ET = { "Електротехника" };
-    discipline BP = { "Базово Програмиране" };
-    discipline OKS = { "Общи Компютърни системи" };
-    discipline Math = { "Математика" };
-    discipline AE = { "Английски" };
-    discipline SSP = { "ССП" };
-    newStudent.disciplines[0] = ET;
-    newStudent.disciplines[1] = BP;
-    newStudent.disciplines[2] = OKS;
-    newStudent.disciplines[3] = Math;
-    newStudent.disciplines[4] = AE;
-    newStudent.disciplines[5] = SSP;
+    discipline disciplines[6];
+    disciplines[0] = { "Електротехника" ,0};
+    disciplines[1] = { "Базово Програмиране", 0};
+    disciplines[2] = { "Общи Компютърни системи" , 0};
+    disciplines[3] = { "Математика" ,0};
+    disciplines[4] = { "Английски" ,0};
+    disciplines[5] = { "ССП" ,0};
 
     for (int i = 0; i < groupCount; i++)
     {
@@ -215,6 +225,7 @@ void printStudents(int selection, student group[]) {
         if (printed == 0)
             cout << "Няма налични студенти"<<endl;
     }
+    system("pause");
 }
 
 void sortStudentsByFN(student group[]) {
@@ -253,76 +264,34 @@ bool addGrades(int fn, student group[]) {
         }
         else
             continue; 
+        system("CLS");
     }
-    if (addedGrades==0)
-    {
-        cout << "Всички оценки на студента са въведени!"<<endl;
-    }
+    if (addedGrades == 0)
+        cout << "Всички оценки на студента са въведени!" << endl;
+    else
+        cout << "Оценките са въведени успешно!"<<endl;
+    system("pause");
     return true;
 }
 
-void writeToFile(student group[]) {
-    out_stream.open(fileName);
-    if (out_stream.fail())
-    {
-        cout << "fail";
-    }
-    for (int i = 0; i < groupCount; i++)
-    {
-        if (group[i].facultyId == 0)
-            continue;
-        out_stream << group[i].facultyId << "," << group[i].name << "," << group[i].sex << "," << group[i].age << "," << group[i].status << ",";
-        for (int j = 0; j < disciplinesCount; j++)
-        {
-            out_stream << group[i].disciplines[j].name << "-" << group[i].disciplines[j].grade<<",";
-        }
-        out_stream << endl;
-    }
-    out_stream.close();
+void writeToFile(student group[30]) {
+    fstream file;
+    file.open(fileName, ios::binary | ios::out);
+    file.write((char*)group, groupCount * (sizeof(student)));
+    file.close();
+        //cout << "Неуспешно отваряне на фаила!" << endl;
 }
 
 void readFromFile(student group[]) {
-    in_stream.open(fileName);
-    int facultyN;
-    string name;
-    string sex;
-    int age;
-    string status;
-    string line;
-    string info;
-    int start = 0;
-    int cuts = 0;
-    while (getline(in_stream, line)) {
-        for (int i = 0; i < line.length(); i++)
-        {
-            if (i == 40)
-            {
-                cout << "d";
-            }
-            if (line[i]==',')
-            {
-                info =line.substr(start, i-start);
-                switch (cuts)
-                {
-                case 0:
-                    facultyN = stoi(info);
-                    break;
-                case 1:
-                    name = info;
-                    break;
-                case 2:
-                    sex = info;
-                    break;
-                case 3:
-                    age = stoi(info);
-                    break;
-                case 4:
-                    status = info;
-                    break;
-                }
-                start = i+1;
-                cuts++;
-            }
-        }
-    }
+    fstream file;
+    file.open(fileName, ios::binary | ios::in);
+    file.seekg(0L, ios::end);
+    long pos = (long)file.tellg();
+    file.seekg(0L, ios::beg);
+    file.close();
+    int n = pos / (sizeof(student));
+    file.open(fileName, ios::binary | ios::in);
+    file.read((char*)group, n * (sizeof(student)));
+    file.close();
+        //cout << "Неуспешно отваряне на фаила!" << endl;
 }
